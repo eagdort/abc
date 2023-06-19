55c6c2e8bb3d8365af58c79acaae5bbe0c47ba21
Magomed Buzurtanov <>
2023-06-19 15:22:51 +0300

    add test

---
+++ src/test/java/ru/eagdort/interview/MultiplicityInterviewTaskTest.java
@@ -0,0 +1,12 @@
+package ru.eagdort.interview;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MultiplicityInterviewTaskTest {
+
+    @Test
+    void check() {
+    }
+}
