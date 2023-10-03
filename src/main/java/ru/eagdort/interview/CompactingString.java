509513c2b9504014396913dde5150f316b7f02e1
Magomed Buzurtanov <buzurtanov@buzurtanov.local>
2023-10-03 15:16:55 +0300

    add task CompactingString

---
+++ src/main/java/ru/eagdort/interview/CompactingString.java
@@ -0,0 +1,19 @@
+package ru.eagdort.interview;
+
+/**
+ * Дана строка типа: "AAABBBACCDDDK".
+ * Функция должна преобразовать строку в результат вида: "A3B3A1C2D3K1"
+ */
+
+public class CompactingString {
+    public static void main(String[] args) {
+        String str = "AAABBBACCDDDK";
+        System.out.println(compact(str));
+
+    }
+
+    private static String compact(String str) {
+        // ...
+        return "";
+    }
+}
